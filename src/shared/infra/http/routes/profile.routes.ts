38fd1b20ea01a34ada1d68740edf2264ca840604
ericknathan <erick.capito@hotmail.com>
2022-03-25 10:22:14 -0300

    Rotas de perfil atualizadas

--- src/shared/infra/http/routes/profile.routes.ts
+++ src/shared/infra/http/routes/profile.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { GetUserProfileController } from '@modules/accounts/useCases/profile/getUserProfile/GetUserProfileController';
+import { UpdateProfileController } from '@modules/accounts/useCases/profile/updateProfile/UpdateProfileController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const profileRoutes = Router();
 
 const getUserProfileController = new GetUserProfileController();
+const updateProfileController = new UpdateProfileController();
 
 profileRoutes.get('/:organization_id/:id', getUserProfileController.handle);
+profileRoutes.post('/:id', ensureAuthenticated, updateProfileController.handle);
 
 export { profileRoutes };
